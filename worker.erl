757dde6c92b57fa7f4d3bafd6da2a7046451de49
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-05-11 09:41:33 +0300

    A distributed suffix array works.

--- worker.erl
+++ worker.erl
@@ -6,44 +6,61 @@
 
 -module(worker).
 -export([
-		sa/2
+		sa/3
 		]).
 
 
 
-
-
-sa(Pid,File) ->
+sa(Fan,Sink,File) ->
 	%% read file and create Bin and sort fun
 	{ok,Bin} = file:read_file(File),
 	Size = size(Bin),
-	Bin_index = fun(N) when N < Size-1 -> binary:at(Bin, N); (_) -> $$ end,
-	F = fun(F,X1,X2) -> Vx = Bin_index(X1), Vy = Bin_index(X2),
-						case Vx =:= Vy of true -> F(F,X1+1,X2+1); _ -> Vx < Vy end
+	Bin_index = fun(N) when N < Size -> binary:at(Bin, N); (_) -> $$ end,
+	F = fun(F,X1,X2) -> V1 = Bin_index(X1), V2 = Bin_index(X2),
+						case V1 =:= V2 of true -> F(F,X1+1,X2+1); _ -> V1 < V2 end
 		end,	
 	Compare = fun(Shift,Ls) -> 
-				lists:sort(fun({_,_,X1},{_,_,X2}) -> F(F,X1+Shift,X2+Shift) end,Ls) 
+				lists:sort(fun(X1,X2) -> F(F,X1+Shift,X2+Shift) end,Ls) 
 	   		  end,
 		
 
 	%% request workload until 'stop' msg comes
-	Pid ! {self(),ready},
+	Fan ! {self(),ready},
+	sa(Fan,Sink,Bin,Compare).
+
+sa(Fan,Sink,Bin,F) ->
 	receive
 		stop -> io:format("worker terminated normally~n");
-		Prefix -> Pid ! {Prefix,Compare(length(Prefix),scan(Prefix,Bin))}
+		Prefix -> 
+			Sink ! { Prefix, F(length(Prefix),scan(Prefix,Bin)) },
+			Fan ! {self(),ready},
+			sa(Fan,Sink,Bin,F)
 	end.
 
 
 % returns a string of pointers
-scan(Prefix,Bin) -> scan(Prefix,Bin,Prefix,0,[]).
+scan(Prefix,Bin) -> 
+	case lists:last(Prefix) of
+		$$ -> 
+			LastPart = binary:part( Bin,{byte_size(Bin),1-length(Prefix)} ),
+			io:format("LastPart:~p~n",[LastPart]),
+			case list_to_binary(lists:droplast(Prefix)) of 
+				LastPart -> [size(Bin)+1-length(Prefix)];
+				_ -> []
+			end;
+		_ -> scan(Prefix,Bin,Prefix,0,[])
+	end.
 
-scan(Prefix,<<S,Bin/binary>>,[S|Rest],N,Acc) -> scan(Prefix,Bin,Rest,N+1,Acc);
-scan(Prefix,Bin,[],N,Acc) -> scan(Prefix,Bin,Rest,N,[N-length(Prefix)|Acc]);
-
-scan(Prefix,<<$$>>,_,N,Acc) -> Acc;
+scan(Prefix,<<>>,_,_,Acc) -> 
+	io:format("WORKER: sequence for ~p scanned. Contains ~p elements~n",[Prefix,length(Acc)]),
+	Acc;
+scan(Prefix,<<S,Bin/binary>>,[S|Rest],N,Acc) -> scan(Prefix,Bin,Prefix,N+1, case match(Bin,Rest) of
+																				true -> [N|Acc];
+																				_ -> Acc
+																			end);
 scan(Prefix,<<_,Bin/binary>>,_,N,Acc) -> scan(Prefix,Bin,Prefix,N+1,Acc).
 
 
-
-
+match(Bin,Str) when size(Bin) < length(Str) -> false; 
+match(Bin,Str) -> binary:part(Bin, 0, length(Str)) =:= list_to_binary(Str).
 
