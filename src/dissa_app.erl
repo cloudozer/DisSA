fa228c536b7d9caa8575c68c22637c13766d9c1c
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-05-11 13:24:20 +0300

    refactored as opt application

---
+++ src/dissa_app.erl
@@ -0,0 +1,16 @@
+-module(dissa_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    dissa_sup:start_link().
+
+stop(_State) ->
+    ok.
